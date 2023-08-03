e6b9a925ae9b1ee7551129ac424135806ed465fa
Gerardo B <brisenobgerardo@gmail.com>
2023-08-02 22:26:44 -0500

    Fixed write to txt file script

--- writeToTxT.c
+++ writeToTxT.c
@@ -1,33 +1,46 @@
+//Se incluyen las librerias standar input/output y string
 #include <stdio.h>
-#include <stdio.h>
+#include <string.h>
 
+//Variables char array para guardar los inputs
 char nombre[40];
 char adeudo[10];
 
-FILE *fp;  //pointer de tipo file
+//Pointer tipo file para el archivo de texto
+FILE *fp;
 
-int agregarCliente(){
+//Funcion para esbribir a archivo .txt
+void agregarCliente(){
 
+    //Abrir el archivo
     fp = fopen("C:\\Users\\angel\\OneDrive\\Documentos\\Proyectos C\\clientes.txt", "a+");
 
-    printf("Ingrese nombre del cliente:");
-    scanf("%40[^ ]", &nombre);
-    fputs(nombre, fp);
-    fputs(", ", fp);
-    
-    printf("Ingrese adeudo: ");
-    scanf("%s", &adeudo);
-    fputs(adeudo, fp);
-    //fputs("\n", fp);
-    //scanf("%39[0-9a-zA-Z ]", &nombre);
-    fputs("\n", fp);
+    //Mostrar mensaje en la consola
+    printf("Ingrese nombre del cliente: ");
+    //Escribir el input obtenido en al array nombre
+    fgets(nombre, sizeof(nombre), stdin);
+    //Cambiar el caracter de nueva línea por el de final de línea
+    nombre[strcspn(nombre, "\n")] = '\0';
 
+    //Mostrar mensaje en la consola
+    printf("Ingrese adeudo: ");
+    //Escribir el input obtenido en al array adeudo
+    fgets(adeudo, sizeof(adeudo), stdin);
+    //Cambiar el caracter de nueva línea por el de final de línea
+    adeudo[strcspn(adeudo, "\n")] = '\0';
+
+    //Escribir el contenido de nombre y adeudo al archivo de texto
+    fprintf(fp, "%s, %s\n", nombre, adeudo);
+
+    //Cerrar el archivo
     fclose(fp);
 
 }
 
+//Funcion principal
 int main(){
 
+    //Correr continuamente la funcion agregarCliente()
     while(1){
         agregarCliente();
     }
